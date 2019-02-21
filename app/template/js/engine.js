23373ea5e70f999b486fdd32f5832b9d95c93c9c
Aleksey <kronos2003@gmail.com>
2019-02-21 12:50:39 +0200

    Добавлен скрипт разворачивания строк в таблеце catalog inner

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -162,3 +162,48 @@ document.querySelector('.apanel .close-menu').addEventListener("click", function
 }, false);
 
 // =/mobile menu
+
+
+
+$(document).on('click', '.table-grid .model-title', function(e){
+	e.preventDefault();
+	let $this = $(this),
+		$modelbody = $this.next('.model-body');
+
+	$this.toggleClass('arrow-down');
+	$modelbody.slideToggle();
+});
+
+
+// `closest` polyfill for IE11
+(function (ElementProto) {
+	if (typeof ElementProto.matches !== 'function') {
+		ElementProto.matches = ElementProto.msMatchesSelector || ElementProto.mozMatchesSelector || ElementProto.webkitMatchesSelector || function matches(selector) {
+			var element = this;
+			var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
+			var index = 0;
+
+			while (elements[index] && elements[index] !== element) {
+				++index;
+			}
+
+			return Boolean(elements[index]);
+		};
+	}
+
+	if (typeof ElementProto.closest !== 'function') {
+		ElementProto.closest = function closest(selector) {
+			var element = this;
+
+			while (element && element.nodeType === 1) {
+				if (element.matches(selector)) {
+					return element;
+				}
+
+				element = element.parentNode;
+			}
+
+			return null;
+		};
+	}
+})(window.Element.prototype);
